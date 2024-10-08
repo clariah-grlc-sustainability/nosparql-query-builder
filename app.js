b7b682e283532c3a460035738f003c054b1f810f
kody.moodley@gmail.com <kody.moodley@gmail.com>
2024-10-08 15:55:53 +0200

    adds expand/collapse button for attribute filters

--- app.js
+++ app.js
@@ -30,6 +30,16 @@ $(document).ready(function() {
         executeSparqlQuery();
     });
 
+    $('.toggle-button').click(function() {
+        const target = $(this).data('target');
+        $(target).slideToggle(); // Toggle visibility of the target section
+    
+        // Change the button text to "+" or "-" depending on the visibility
+        const buttonText = $(this).text() === "−" ? "+" : "−";
+        $(this).text(buttonText);
+
+    });
+
     $(document).on('change', '.predicate, .min-val, .max-val, .regex, .object, #show-attribute, #limit-val', function() {
         updateSparqlQuery();
         newItems = [];                                                  // Refresh dropdown items
@@ -185,7 +195,7 @@ function addSearchFilter() {
         </div>
     `;
 
-    $('#filter-container').append(filter);
+    $('#attribute-filters-section').append(filter);
     setupAutocomplete(`#predicate-${filterCount}`, 'predicate');
     setupAutocomplete(`#object-${filterCount}`, 'object');
     filterCount++;
